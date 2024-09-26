f339dafa641d8a4565e012f81dcb476bca8db423
Matheus-Uchoa/ <matheusuchoa@unitins.br>
2024-09-25 21:18:04 -0300

    feat: criando Camada DTO para melhor tratamento dos dados

---
+++ leilao-api/src/main/java/br/com/selecao/locadora/dto/UnidadeDTO.java
@@ -0,0 +1,16 @@
+package br.com.selecao.locadora.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+public class UnidadeDTO {
+    private String nome;
+}
